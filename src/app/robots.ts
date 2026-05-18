909c287940d88bfabe488ba5b4cdc1ffc241beaa
shareef6907 <events.photos.videos@gmail.com>
2026-05-18 18:11:45 +0300

    seo: add /api, /_next, /admin disallows to robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: ['/api/', '/_next/', '/admin/', '/*.json$'],
     },
     sitemap: 'https://filmproductionbahrain.com/sitemap.xml',
   }
